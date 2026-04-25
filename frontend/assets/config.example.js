a3c9159fbd59740ad512c6cc9419eb6a14a5dc7a
lewbox18-eng <lewbox18@gmail.com>
2026-04-25 22:32:57 +0100

    Delete frontend directory

--- frontend/assets/config.example.js
+++
@@ -1,6 +0,0 @@
-window.GitShopConfig = Object.freeze({
-  shopName: "NetMore",
-  apiBaseUrl: "https://your-backend.example.com",
-  shopUrl: "https://your-shop.example.com",
-  currency: "USD"
-});
--- frontend/assets/config.js
+++
@@ -1,6 +0,0 @@
-window.GitShopConfig = Object.freeze({
-  shopName: "NetMore",
-  apiBaseUrl: "https://https://netmoresmp.up.railway.app",
-  shopUrl: "https://lewbox18-eng.github.io/NetMoreSite",
-  currency: "USD"
-});
